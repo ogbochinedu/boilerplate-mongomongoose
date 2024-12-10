4e691ea07a776eee2007c0faf431c0ffa042cb6c
macbook <ogbochinedu95@gmail.com>
2024-12-10 05:13:51 +0100

    updates

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 
 let Person;
-
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch((error) => console.error("Connection error:", error));
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
