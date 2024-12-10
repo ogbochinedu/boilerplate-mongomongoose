7017b960024df84151a470cd292393c874bc5f21
macbook <ogbochinedu95@gmail.com>
2024-12-10 06:02:09 +0100

    uo

--- myApp.js
+++ myApp.js
@@ -95,7 +95,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findOneAndRemove({ _id: personId }, (err, person) => {
+    if (err) return done(err);
+    done(null, person);
+  });
 };
 
 const removeManyPeople = (done) => {
